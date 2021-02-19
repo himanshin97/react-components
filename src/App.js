4fd0d1a56cc3f0f400646d996239b4f89bb6bdb0
himanshin97 <himanshi.n97@gmail.com>
2021-02-19 14:30:42 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import logo from './logo.svg';
-//import image from './green-tick.png';
+import image from './green-tick.png';
 import { useState } from 'react';
 import Select from 'react-select';
+import Option from 'react-select';
 import { Checkmark } from 'react-checkmark'
 import './App.css';
-import { customerData } from './data';
+//import { customerData } from './data';
 import { options } from './data';
 import {
   Box,
@@ -21,42 +22,69 @@ import {
 
 function App() {
 
-  const [selectedOption,setSelectedOption] = useState(null);
+  const [selectedOption,setSelectedOption] = useState(options[0].value);
+  const [images,setImages] = useState(null)
   console.log(selectedOption);
 
-  const styles = {
-    fontSize: 14,
-    color: 'blue',
-    borderRadius: 4,
+  {/*const handleChange = (event) => {
+    console.log(event)
+    if (event.value === options) {
+      setSelectedOption({ selectedOption},<div><img src = {image} /></div>)
+    } else {
+      setSelectedOption(null)
+    }
+    console.log(selectedOption)
+  } */}
+
+  const handleChange = (selectedOption,images) => {
+    setSelectedOption({selectedOption,images});
     
+  }
+
+ 
+  
+
+  const customStyles={
+    option: (provided,state)=>({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: state.isSelected ? 'red': 'blue',
+      padding: 20
+    }
+
+    )
   }
 
   return (
     <>
     <div className="App">
       <Flex>
-    <Box   p={3}
-  fontSize={3}
-  width={[ 1, 1, 1/2 ]}
-  color='Green'
-  bg='secondary'
+      <Box   p={3}
+            fontSize={3}
+            width={[ 1, 1, 1/2 ]}
+            color='Green'
+            bg='secondary'
+            
   >
-     <h1 font-size="12px" align="left">Welcome to the Bank</h1>
-    
-      <Select border-color="green"
-      onChange={(selectedOption)=> setSelectedOption({selectedOption})} 
-      options={options}
-      styles={styles}
-      placeholder="Select Customer" 
-      isSearchable 
+     <h1 fontSize="12px" align="left">Welcome to the Bank</h1>
+     <Select border-color="green"
+        options={options}
+        onChange={handleChange}
+        classNamePrefix="lp-copy-sel"
+        placeholder="Select Customer" 
+        isSearchable 
+        isClearable
+        styles={customStyles}
        />
+      
+       
        
        </Box>
        </Flex>
        
                 
       
-                </div>
+    </div>
     </>
   );
 
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 import image from './green-tick.png';
 
 export const options = [
-    { value: 'gs kumar', label: <div>11-11-11   111111111   Savings Account    GS Kumar <img src={image} height="30px" width="30px"/></div> },
-    { value: 'pk shrivastava', label: <div>22-22-22   222222222   Current Account   PK Shrivastava <img src={image} height="30px" width="30px"/> </div> },
-    { value: 'divya nigam', label: <div>33-33-33   333333333   Current Account   Divya Nigam <img src={image} height="30px" width="30px"/></div> },
-    { value: 'yash bhagtnagar', label: <div>44-44-44   444444444   Savings Account   Yash Bhagtnagar <img src={image} height="30px" width="30px"/></div> },
-    { value: 'purva jain', label: <div>55-55-55   555555555   Savings Account   Purva Jain <img src={image} height="30px" width="30px"/></div> }
+    { value: 'gs kumar', label: <div>11-11-11   111111111   <br/>Savings Account - GS Kumar &nbsp; <img src={image} height="50px" width="30px"/> </div>},
+    { value: 'pk shrivastava', label: "22-22-22   222222222  \nCurrent Account - PK Shrivastava  " },
+    { value: 'divya nigam', label: "33-33-33   333333333  \nCurrent Account - Divya Nigam " },
+    { value: 'yash bhagtnagar', label: <div>44-44-44   444444444  <br/>Savings Account - Yash Bhagtnagar  &nbsp; <img src={image} height="50px" width="30px"/> </div> },
+    { value: 'purva jain', label: <div>55-55-55   555555555   <br/> Account - Purva Jain &nbsp; <img src={image} height="50px" width="30px"/> </div> }
   ];
 
 export const customerData = [
